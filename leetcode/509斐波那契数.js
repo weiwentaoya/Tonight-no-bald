9246940f85b6926c5442b07be7f3804f404d7a02
weiwentao <17600626753@163.com>
2021-11-12 09:43:41 +0800

    509

--- leetcode/509斐波那契数.js
+++ leetcode/509斐波那契数.js
@@ -1,19 +1,15 @@
 var fib = function(n) {
-    const obj = {}
+    const obj = []
     for (let i = 0; i <=n; i++) {
-        if (i === 0) {
-            obj[i] = 0
-        } else if (i === 1) {
-            obj[i] = 1
-        }else{
-            const n =  obj[i-2]+ obj[i-1]
-            obj[i] =n>( 1e9+7)?n%(1e9+7):n
+        if (i < 2) {
+            obj[i] = i
+        } else{
+            obj[i] = obj[i-2]+ obj[i-1]
         }
     }
-    console.log(obj[n]);
     
     return obj[n]
 };
 
-fib(45)
-console.log(1134903170%(1e9+7));
+
+console.log(fib(45));
